f4ad37b51d3f57b66d942ce01b46482c0a382c2f
Robin Klaassen <robin.klaassen@alliander.com>
2024-12-09 14:18:54 +0100

    finished day 9

--- day09/main.py
+++ day09/main.py
@@ -1,7 +1,9 @@
+from itertools import groupby
+
 from utils import read_input
 
 TEST_ANSWER_PART1 = 1928
-TEST_ANSWER_PART2 = ...
+TEST_ANSWER_PART2 = 2858
 
 
 class DiskSpace:
@@ -60,28 +62,91 @@ def part1(lines: list[str]) -> int:
 
     return checksum
 
-    # # make entire disk with ints and Nones
-    # disk = []
-    # for i, x in enumerate(line):
-    #     if i % 2 == 0:
-    #         ...
-    #
-    # # make a deque of all file blocks
-    # file_block_sizes = [int(x) for i, x in enumerate(line) if i % 2 == 0]
-    #
-    # file_blocks = deque()
-    # for i, s in enumerate(file_block_sizes):
-    #     file_blocks += s * [i]
-    #
-    # total_block_count = sum(file_block_sizes)
-    # checksum = 0
-    # for i in range(total_block_count):
-    #     # determine if index is originally occupied in the line (then popleft) or empty (then popright)
-    #     ...
+
+def build_blocks(line: str) -> list[int | None]:
+    blocks = []
+    file_id = 0
+
+    for i, x in enumerate(line):
+        size = int(x)
+        if i % 2 == 0:
+            # file
+            value = file_id
+            file_id += 1
+        else:
+            # space
+            value = None
+
+        blocks += size * [value]
+
+    return blocks
+
+
+def get_free_space_locations(blocks: list[int | None]) -> dict[int, int]:
+    # dict of starting index to size of empty space
+    output = {}
+    index = 0
+    for k, v in groupby(blocks):
+        contiguous_size = len(list(v))
+        if k is None:
+            output[index] = contiguous_size
+        index += contiguous_size
+
+    return output
+
+
+def remove_all_occurrences(blocks: list[int | None], value: int):
+    # set occurrences to zero, mutates original list
+    while True:
+        try:
+            idx = blocks.index(value)
+            blocks[idx] = None
+        except ValueError:
+            break
+
+
+def print_blocks(blocks: list[int | None]):
+    output = ""
+    for el in blocks:
+        output += (str(el) if el is not None else ".")
+    print(output)
 
 
 def part2(lines: list[str]) -> int:
-    ...
+    assert len(lines) == 1
+    line = lines[0]
+    blocks = build_blocks(line)
+    file_block_sizes = [int(x) for i, x in enumerate(line) if i % 2 == 0]
+
+    while file_block_sizes:
+        file_id = len(file_block_sizes) - 1
+        file_size = file_block_sizes.pop()
+        free_space = get_free_space_locations(blocks)
+
+        for idx, size in free_space.items():
+            if size < file_size:
+                continue
+
+            if idx > blocks.index(file_id):
+                # do not move files to the right!
+                continue
+
+            # move the file
+            remove_all_occurrences(blocks, file_id)
+            for s in range(file_size):
+                blocks[idx + s] = file_id
+
+            break
+
+    # calculate checksum
+    checksum = 0
+    for i, x in enumerate(blocks):
+        if x is None:
+            continue
+
+        checksum += i * x
+
+    return checksum
 
 
 if __name__ == "__main__":
